a4d051ee34b988a3dcda5b730f6710dd5232ade0
Pavel <opatriarkh@gmail.com>
2026-03-09 02:48:17 +0300

    feat: add worker threads and child processes

--- src/cp/execCommand.js
+++ src/cp/execCommand.js
@@ -1,10 +1,20 @@
+import { spawn } from "child_process";
+
 const execCommand = () => {
-  // Write your code here
-  // Take command from CLI argument
-  // Spawn child process
-  // Pipe child stdout/stderr to parent stdout/stderr
-  // Pass environment variables
-  // Exit with same code as child
+  const command = process.argv[2];
+  const [cmd, ...args] = command.split(" ");
+
+  const child = spawn(cmd, args, {
+    env: process.env,
+    shell: true,
+  });
+
+  child.stdout.pipe(process.stdout);
+  child.stderr.pipe(process.stderr);
+
+  child.on("exit", (code) => {
+    process.exit(code ?? 0);
+  });
 };
 
 execCommand();
